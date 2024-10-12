c7233ee15287193081471cb71d395fc4f220f63b
Nuttapong Klaokliang <nuttapong95.klng@gmail.com>
2024-10-12 20:56:33 +0700

    Move value in for_each to variables.tf

--- variables.tf
+++ variables.tf
@@ -15,3 +15,45 @@ variable "aws_region" {
     type        = string
     default     = "ap-southeast-1"
 }
+
+variable "sqs_queues" {
+    description = "A map of SQS queues to create"
+    type    = map(object({
+        name                        = string
+        delay_seconds               = number
+        max_message_size            = number
+        message_retention_seconds   = number
+        receive_wait_time_seconds   = number
+        visibility_timeout_seconds  = number
+        fifo_queue                  = bool
+        content_based_deduplication = bool
+        kms_master_key_id           = string
+        kms_data_key_reuse_period_seconds = number
+    }))
+    default = {
+        queue1 = {
+            name                        = "my-sqs-queue-1"
+            delay_seconds               = 0
+            max_message_size            = 262144
+            message_retention_seconds   = 345600
+            receive_wait_time_seconds   = 0
+            visibility_timeout_seconds  = 30
+            fifo_queue                  = false
+            content_based_deduplication = false
+            kms_master_key_id           = null
+            kms_data_key_reuse_period_seconds = null
+        },
+        queue2 = {
+            name                        = "my-sqs-queue-2"
+            delay_seconds               = 10
+            max_message_size            = 262144
+            message_retention_seconds   = 345600
+            receive_wait_time_seconds   = 0
+            visibility_timeout_seconds  = 30
+            fifo_queue                  = false
+            content_based_deduplication = false
+            kms_master_key_id           = null
+            kms_data_key_reuse_period_seconds = null
+        }
+    }
+}
